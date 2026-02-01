799a720c7dd1bd6b134cb386df55e69240401ae4
WilliamFreed <william.freed@me.com>
2026-02-01 08:10:00 -0500

    fixed camera shake

--- Assets/Engine/Scripts/Camera/CameraShake.cs
+++ Assets/Engine/Scripts/Camera/CameraShake.cs
@@ -9,27 +9,20 @@ public class CameraShake : MonoBehaviour
 
     void Awake()
     {
-        originalPos = transform.localPosition;
+        
     }
 
     void Update()
     {
         if (shakeTime > 0)
         {
+            originalPos = transform.localPosition;
             float x = (Mathf.PerlinNoise(Time.time * frequency, 0f) - 0.5f) * 2f;
             float y = (Mathf.PerlinNoise(0f, Time.time * frequency) - 0.5f) * 2f;
 
             transform.localPosition = originalPos + new Vector3(x, y, 0) * shakeStrength;
 
             shakeTime -= Time.deltaTime;
-        }
-        else
-        {
-            transform.localPosition = Vector3.Lerp(
-                transform.localPosition,
-                originalPos,
-                Time.deltaTime * 15f
-            );
         }
     }
 
